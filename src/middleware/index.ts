f22f310bafb9d9368c4a06093c2ce97898c6cb7c
Klaus Paiva <kpaiva@atlassian.com>
2018-12-27 16:16:37 +1100

    Introduce a Map with some smarts around the underlying object always being an array

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,25 @@
 import { Action } from './types';
 
-const actionsMap = new Map<object, Action[]>();
+class CustomMap<K, V> extends Map {
+  set(key: K, action: V) {
+    const currentValue = super.get(key);
+    if (Array.isArray(currentValue)) {
+      super.set(key, currentValue.concat([action]));
+    } else {
+      super.set(key, [action]);
+    }
+    return this;
+  }
+  get(key: K) {
+    const currentValue = super.get(key);
+    if (Array.isArray(currentValue)) {
+      return currentValue;
+    } else {
+      return [];
+    }
+  }
+}
+
+const actionsMap = new CustomMap<any, Action[]>();
 
 export default actionsMap;
