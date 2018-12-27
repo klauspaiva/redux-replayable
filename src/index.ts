d49fd3379bb6bdcc48a0e9b9efc216925ca2b6ad
Klaus Paiva <kpaiva@atlassian.com>
2018-12-27 15:50:48 +1100

    Prettier with a pre-commit hook

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import actionsMap from "./middleware";
+import actionsMap from './middleware';
 
 export const middleware = (store: any) => (next: any) => (action: any) => {
   console.group(action.type);
   console.info(actionsMap.set(store, [action]));
-  console.info("dispatching action", action);
+  console.info('dispatching action', action);
   let result = next(action);
-  console.log("next state", store.getState());
+  console.log('next state', store.getState());
   console.info(actionsMap);
   console.groupEnd();
   return result;
